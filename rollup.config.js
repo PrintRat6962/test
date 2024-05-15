ef7edeaa89741da5285b9a0daea37e940cdac5be
Christian Reyes <printrat8624@gmail.com>
2024-05-15 14:46:14 -0500

    Path editing for gh-pages

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import css from 'rollup-plugin-css-only';
+import replace from '@rollup/plugin-replace';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -38,6 +39,12 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
+		replace({
+			'process.env.NODE_ENV': JSON.stringify(production ? 'production' : 'development'),
+			'preventAssignment': true,
+			// Set base path for GitHub Pages
+			'process.env.BASE_PATH': JSON.stringify(production ? '/Portfolio/' : '/')
+		  }),
 		svelte({
 			compilerOptions: {
 				// enable run-time checks when not in production
